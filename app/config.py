812a7ed516011a861b116cb473d662f56d8909dd
SergeyRO <sergey_r.o@mail.ru>
2023-03-07 21:43:13 +0300

    fff

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 import os
 
-# CELERY_BROKER = os.getenv("CELERY_BROKER")
-# MONGO_DSN = os.getenv("MONGO_DSN")
-MONGO_DSN="mongodb://app:1234_secret@127.0.0.1:27017/files?authSource=admin"
-CELERY_BROKER="redis://127.0.0.1:6379/4"
-CELERY_BACKEND="redis://127.0.0.1:6379/2"
+CELERY_BROKER = os.getenv("CELERY_BROKER")
+MONGO_DSN = os.getenv("MONGO_DSN")
+CELERY_BACKEND = os.getenv("CELERY_BACKEND")
+# MONGO_DSN="mongodb://app:1234_secret@127.0.0.1:27017/files?authSource=admin"
+# CELERY_BROKER="redis://127.0.0.1:6379/4"
+# CELERY_BACKEND="redis://127.0.0.1:6379/2"
 
 #PG_DSN = os.getenv("PG_DSN")
