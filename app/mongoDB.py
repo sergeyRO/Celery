976ceba5e14f1add4c8570112fcc3e604f785ff8
SergeyRO <sergey_r.o@mail.ru>
2023-03-12 18:39:38 +0300

    gh

--- app/mongoDB.py
+++ app/mongoDB.py
@@ -1,7 +1,7 @@
 from config import MONGO_DSN
 import pymongo
 from bson.binary import Binary
-
+MONGO_DSN="mongodb://admin:password@127.0.0.1:27017/"
 client = pymongo.MongoClient({MONGO_DSN}, serverSelectionTimeoutMS=5000)
 try:
     print(client.server_info())
