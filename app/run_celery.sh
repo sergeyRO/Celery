c5b7e2668afaa66876d26a215d7aaf1ee9fa69fb
SergeyRO <sergey_r.o@mail.ru>
2023-03-17 22:48:12 +0300

    gh

--- app/run_celery.sh
+++ app/run_celery.sh
@@ -1,3 +1,1 @@
 celery -A celery_app worker -l INFO --pool=solo --concurrency=10 -n worker1@localhost
-celery -A celery_app worker -l INFO --pool=solo --concurrency=10 -n worker2@localhost
-celery -A celery_app worker -l INFO --pool=solo --concurrency=10 -n worker3@localhost
